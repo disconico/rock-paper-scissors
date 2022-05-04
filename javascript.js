e39877b762a0739021337c9c0b1755f44f4863d3
Nico <disconico@outlook.com>
2022-05-04 15:40:45 +0200

    Add first part of player selection

--- javascript.js
+++ javascript.js
@@ -3,9 +3,28 @@
 // Define computer behavior by randomly returning one of the 3 arguments
 
 function computerPlay(){
-    var computerPossibilities = ['Rock', 'Paper', 'Scissors'];
-    var computerChoice = computerPossibilities.length;
-    var computerPlay = computerPossibilities[Math.floor(Math.random()*computerChoice)];
+    let computerPossibilities = ['Rock', 'Paper', 'Scissors'];
+    let computerChoice = computerPossibilities.length;
+    let computerPlay = computerPossibilities[Math.floor(Math.random()*computerChoice)];
     console.log('The computer played: '+computerPlay);
 }
 console.log(computerPlay());
+
+
+//Get user's input > Make it case-insensitive !
+//#Define a variable to store input
+function playerPlay () {
+    let playerChoice = document.getElementById("text").value;
+    console.log(playerChoice);
+};
+
+
+
+//#Get input and verify that the input is right (rock paper or scissors)
+
+
+
+
+//Define the winner
+//If someone's win
+//If draw
