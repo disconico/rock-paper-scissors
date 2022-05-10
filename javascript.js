a441949bd1280c03b0731272db7899111d8e5b3e
Nico <disconico@outlook.com>
2022-05-10 11:20:31 +0200

    Clean RPS files

--- javascript.js
+++ javascript.js
@@ -1,80 +1,71 @@
-// Let's go !
-
-//Define choices for the game 
-const choices = ['Rock', 'Paper', 'Scissors']
+// Rock Paper Scissors game
 
 //Create a random outcome for the computer - Rock, Paper, or Scissors
 function computerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random()*choices.length)].toLowerCase();
 }
 
 //Get input from player
 function playerChoice() {
     let input = prompt("Type 'Rock', 'Paper', or 'Scissors'").toLowerCase();
-    if (input.includes("rock") || input.includes("paper") || input.includes("scissors")) { ;
+    if (input.includes("rock") || input.includes("paper") || input.includes("scissors")) { 
+        return input;
     } else {
         alert ("Please chose wisely");
         input = prompt("Type 'Rock', 'Paper', or 'Scissors'").toLowerCase();
+        return input
     }
-    return input
-}    
-
-//Define parameters
-let playerSelection = playerChoice();
-let computerSelection = computerChoice();
-
-console.log(playerSelection);
-console.log(computerSelection);
-
-//Define round rules 
-function playRound(){
-    if (playerSelection === computerSelection) {
-        return ("tie");
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return ("player");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return ("player");
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return ("player");
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        return ("computer");
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-        return ("computer");
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        return ("computer");
-    }
-}
-
-//Announce round result
-let roundResult = playRound();
-if (roundResult === "tie") {
-    console.log("It's a tie, no one scores!") ;
-} else if (roundResult === "player") {
-    console.log(`You won, ${playerSelection} beats ${computerSelection} !`);
-} else if (roundResult === "computer") {
-    console.log(`You lost, ${computerSelection} beats ${playerSelection} !`);  
-}
+} 
 
 //Scores
 let playerScore = 0;
 let computerScore = 0;
 
-//Scoreboard update
-if (roundResult === "tie") {
-} else if (roundResult === "player") {
-    playerScore++;
-} else if (roundResult === "computer") {
-    computerScore++;  
+//Define round rules 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie, no one scores!") ;
+        return ("tie");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        console.log(`You won, ${playerSelection} beats ${computerSelection} !`) ;
+        return ("player");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        console.log(`You won, ${playerSelection} beats ${computerSelection} !`) ;
+        return ("player");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        console.log(`You won, ${playerSelection} beats ${computerSelection} !`) ;
+        return ("player");
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerScore++;
+        console.log(`You lost, ${computerSelection} beats ${playerSelection} !`) ;  
+        return ("computer");
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
+        console.log(`You lost, ${computerSelection} beats ${playerSelection} !`) ;  
+        return ("computer");
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
+        console.log(`You lost, ${computerSelection} beats ${playerSelection} !`) ;  
+        return ("computer");
+    }
 }
 
-console.log(playerScore);
-console.log(computerScore);
-
-
-//Define game rules
-while ((playerScore < 5) || (computerScore < 5)) {
-    playRound();
-    break;
+//Define game function
+function game () {
+    while (playerScore < 5 && computerScore < 5) {
+        playRound(playerChoice(), computerChoice());
+        console.log(`The score is ${playerScore} - ${computerScore}`);
+    } 
+    if (playerScore === 5) {
+        console.log("Congrats ! you won !");
+    } else if (computerScore === 5) {
+        console.log("Damn! Try again")
+    }   
 }
 
-
+//Play the game
+game();
