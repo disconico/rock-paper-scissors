1b840be183237ea0a5c70282e04b82d2129e1e07
Nico <disconico@outlook.com>
2022-05-11 09:56:33 +0200

    Add images to selections and end musics

--- javascript.js
+++ javascript.js
@@ -1,21 +1,31 @@
 // Rock Paper Scissors game
 
+//Add music to the game
+const audio = new Audio('./music/background_music.mp3');
+const gameOver = new Audio('./music/game_over.mp3');
+const victory = new Audio('./music/victory.mp3');
+
 //Assign all elements
 const buttons = document.querySelectorAll('.button');
 const resultText = document.querySelector('.round-result-text');
 const scores = document.querySelector('.game-scores');
 const buttonPlayAgain = document.querySelector('.play-again');
-const rounds = document.querySelector('.round-number-text')
+const rounds = document.querySelector('.round-number-text');
+const imageComputer = document.querySelector('.weapon-computer');
+const imagePlayer = document.querySelector('.weapon-player');
 
 let playerScore = 0;
 let computerScore = 0;
+let choice = '';
 let round = 0;
 
 //Launch game when input from user
 buttons.forEach(button => button.addEventListener('click', function(e) {
     playerSelection = e.target.id ;
+    // audio.play();
     countRound();
     countScore(playerSelection, computerChoice());
+    pushPlayerImage(playerSelection);
     endGame();
     resetGame();
 })
@@ -28,10 +38,23 @@ function countRound () {
     return round;
 }
 
-//Create a random outcome for the computer - Rock, Paper, or Scissors
+//Create a random outcome for the computer - Rock, Paper, or Scissors and push the result as an image
 function computerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    return choices[Math.floor(Math.random()*choices.length)].toLowerCase();
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const computerWeapon = choices[Math.floor(Math.random()*choices.length)].toLowerCase();
+    imageComputer.src = `./images/${computerWeapon}.png`;
+    return computerWeapon;
+}
+
+//Push player image
+function pushPlayerImage(playerWeapon) {
+    if (playerWeapon === 'rock') {
+        imagePlayer.src = `./images/rock.png`;
+    } else if (playerWeapon === 'paper') {
+        imagePlayer.src = `./images/paper.png`;
+    } else if (playerWeapon === 'scissors') {
+        imagePlayer.src = `./images/scissors.png`;
+    }
 }
 
 //Define scoring method
@@ -62,11 +85,13 @@ function countScore (playerSelection, computerSelection){
 function endGame () {
     if (computerScore === 5) { 
         scores.textContent = `GAME OVER! It seems @disconico made me too smart!`;    
+        gameOver.play();
         buttons.forEach((button) => {
             button.setAttribute('disabled','');
         });
     } else if (playerScore === 5) {
         scores.textContent = `YOU WON! You are smarter than the computer!`;    
+        victory.play();
         buttons.forEach((button) => {
             button.setAttribute('disabled','');
         })
